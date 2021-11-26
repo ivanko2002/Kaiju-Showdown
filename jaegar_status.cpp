b92fb1ec1d50afd17a6c37aec57adc1596e79497
iivvaannkko <93305471+ngodelaynomore@users.noreply.github.com>
2021-11-26 19:52:50 +0800

    Add files via upload

---
+++ jaegar_status.cpp
@@ -0,0 +1,22 @@
+// Return the status of jaegar
+// Take input from player's data and outputs relevant information
+
+
+#include <iostream>
+#include "struct.h"
+
+using namespace std;
+
+void jstatus(Player1 &j)
+{
+    cout << "Name: " << j.name << endl;
+    cout << "Type: " << j.type << endl;
+    cout << "Health Point: " << j.hp << endl;
+    cout << "Attack Power: " << j.ap << endl;
+    cout << "Experience Point: " << j.xp << endl;
+    cout << "Defense Point: " << j.dp << endl;
+    cout << "Props: " << j.prop << endl;
+}
+
+
+
---
+++ kaiju_status.cpp
@@ -0,0 +1,21 @@
+// Return the status of kaiju
+// Take input from player's data and outputs relevant information
+
+
+#include<iostream>
+#include "struct.h"
+
+using namespace std;
+
+void kstatus(Player2 &k)
+{
+    cout << "Name: " << k.name << endl;
+    cout << "Type: " << k.type << endl;
+    cout << "Health Point: " << k.hp << endl;
+    cout << "Attack Power: " << k.ap << endl;
+    cout << "Experience Point: " << k.xp << endl;
+    cout << "Defense Point: " << k.dp << endl;
+    cout << "Props: " << k.prop << endl;
+}
+
+
