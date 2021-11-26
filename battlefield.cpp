646ac05de59789682990002b185c5785764edce7
iivvaannkko <93305471+ngodelaynomore@users.noreply.github.com>
2021-11-26 18:58:12 +0800

    Add files via upload

---
+++ battlefield.cpp
@@ -0,0 +1,48 @@
+//genreate a random battlefield by a random number
+
+#include <iostream>
+#include "struct.h"
+
+using namespace std;
+
+string battlefield(int n, Player1 &j, Player2 &k){
+
+	if (n==1){
+
+		cout << "The battlefield is on Earth" << endl;
+		cout << "Jaegar's HP + 40, XP + 40" << endl;
+		cout << "Kaiju's HP + 20, XP +20" <<endl;
+		j.hp += 40
+		j.xp += 40;
+		k.hp += 20
+		k.xp += 20;
+
+	}
+
+	else if (n==2){
+
+		cout << "The battlefield is in the ocean" << endl;
+		cout << "Kaiju moves much faster than Jaegar in water" << endl;
+		cout << "Kaiju's AP + 50" << endl;
+		cout << "Jaegar's AP - 40" << endl;
+		k.ap += 50
+		j.ap -= 40;
+
+		}
+
+	else {
+		cout << "The battlefield is space" << endl;
+		cout << "Insufficient oxygen for the pilots" << endl;
+		cout << "Jaegar's HP - 100" << endl;
+		cout << "Kaiju's AP + 40" << endl;
+		j.hp -= 100;
+		k.ap += 40;
+	}
+
+}
+
+       	
+
+
+
+
---
+++ kaiju_props.cpp
@@ -0,0 +1,79 @@
+//Exchange Kaiju's props by xp
+       	
+#include <iostream>
+#include <string>
+#include "struct.h"
+
+using namespace std;
+
+void kprops(Player2 &k)
+{
+	int choice;
+	string prop;
+	while (true){	
+		cout << "Remaining XP" <<k.xp << endl;
+
+		cout << "Powerful organs available:"  << endl;
+		cout << endl; 
+
+		cout << "1. Blue Acid " << endl;
+		cout << "Description: Toxic agent in the blood of Kaiju" << endl;
+		cout << "Effect: Kaiju's HP +100" << endl;
+		cout << "XP in exchange: 30" << endl;
+		cout << endl; 
+
+		cout << "2. Deadly spiked tails" << endl;
+		cout << "Description: 3 killing tails extending Kaiju's chest spike " << endl;
+		cout << "Effect: Jaegar's AP +150" << endl;
+		cout << "XP in exchange: 70" << endl;
+		cout << endl;
+
+
+		cout << "3. Electronic lobes" << endl;
+		cout << "Description: Generate a huge electronic-killing pulse" << endl;
+		cout << "Effect: Freeze Jaegar for 3 rounds" << endl;
+		cout << "XP in exchange: 90" << endl;
+		cout << endl;
+
+	
+		cin >> choice;
+		
+		switch(choice){
+
+			case 1:
+				if (k.xp < 30) {
+					cout << "Insufficient XP!" << endl;
+					continue;
+				}
+				prop = "Blue Acid";
+				k.xp -= 30;
+				break;
+
+			case 2:
+				if (k.xp < 50) {
+					cout << "Insufficient XP!" << endl;
+					continue;
+				}
+				prop = "Deadly spiked tails";
+				k.xp -= 70;
+				break;
+			
+			case 3:
+				if (k.xp < 70) {
+					cout << "Insufficient XP!" << endl;
+					continue;
+				}
+				prop = "Electronic lobes";
+				j.xp -= 90;
+				break;
+
+			default:
+				cout << "Invalid input!" << endl << "Please input a number between 1 - 3" << endl;
+				cout << "Your input is ";
+		
+			}
+	}
+}
+
+
+
