2ee2046bfdf0d01f668e0782e69ebf3ba23e58ab
Yu Hin <93305800+yhc369@users.noreply.github.com>
2021-11-29 02:16:36 +0800

    .

--- main.cpp
+++ main.cpp
@@ -4,22 +4,42 @@ using namespace std;
 
 int main()
 {
-	kchoice();
-	cout << "Welcome to Kaiju Showdown\n";
-	system("PAUSE");
-	int flag = 0;
-	do
-	{
+	int exit_flag=0;
+	while(exit_flag==0){
+		cout << "Welcome to Kaiju Showdown\n";
+		system("PAUSE");
+		int menu_flag = 0;
 		system("CLS");
-		int input, flag = 0;
-		cout << "MENU" << endl;
-		cout << "1.Start game" << endl;
-		cout << "2.Instructions" << endl;
-		cout << "3.Characters info" << endl;
-		cin >> input;
-		if (input < 1 or input > 3)
+		do
 		{
-			cout << "Input out of range, please try again\n";
+			int input, flag = 0;
+			cout << "MENU" << endl;
+			cout << "1.Start game" << endl;
+			cout << "2.Instructions" << endl;
+			cout << "3.Characters info" << endl;
+			cout<< "4.Exit"
+			cin >> input;
+			if (input < 1 or input > 4)
+			{
+				cout << "Input out of range, please try again\n";
+			}
+			else
+			{
+				menu_flag=1;
+			}
+		} while (menu_flag == 0);
+		switch(input){
+			case 1:
+			case 2:
+				printinstructions();
+				system("pause");
+			case 3:
+				printchara();
+				system('pause')
+			case 4:
+				exit_flag=1;
+				break
 		}
-	} while (flag == 0);
+	}
+	
 }
