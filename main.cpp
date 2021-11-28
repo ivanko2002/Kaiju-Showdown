ad2c45e6bf73470d440552c0a25d21c6acf3d011
Yu Hin <93305800+yhc369@users.noreply.github.com>
2021-11-28 22:42:03 +0800

    .

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
-	cout<<"hello";
+	cout<<"Welcome to Kaiju Showdown\n";
+	system("PAUSE");
+	int flag=0;
+	do {
+		system("CLS");
+		int input,flag=0;
+		cout<<"MENU"<<endl;
+		cout<<"1.Start game"<<endl;
+		cout<<"2.Instructions"<<endl;
+		cout<<"3.Characters info"<<endl;
+		cin>>input;
+		if (input<1 or input>3){
+			cout<<"Input out of range, please try again\n";
+	}
+	}	while(flag==0);
 }
