7b0fd6a8848675ed48148add9b8a7288bba7dfe3
iivvaannkko <93305471+ngodelaynomore@users.noreply.github.com>
2021-12-01 09:49:21 +0800

    Update jaegar_props.cpp

--- jaegar_props.cpp
+++ jaegar_props.cpp
@@ -13,28 +13,24 @@ void jprops(Player1 &j){
 	int choice;
 	string jprop;
 	while (true){	
-		cout << "Remaining XP" << j.xp << endl;
+		cout << "Remaining XP" << j.xp << endl << endl;
 
-		cout << "Weapons available:"  << endl;
-		cout << endl; 
+		cout << "Weapons available:"  << endl << endl; 
 
 		cout << "1. I-19 Plasmacaster " << endl;
 		cout << "Description: A particle dispersal cannon that fired a beam of charged plasma" << endl;
 		cout << "Effect: Kaiju's HP -100" << endl;
-		cout << "XP in exchange: 30" << endl;
-		cout << endl; 
+		cout << "XP in exchange: 30" << endl << endl; 
 
 		cout << "2. Plasma Chainsaw" << endl;
 		cout << "Description: Dual weapons infused with plasma" << endl;
 		cout << "Effect: Jaegar's AP +50" << endl;
-		cout << "XP in exchange: 50" << endl;
-		cout << endl;
+		cout << "XP in exchange: 50" << endl << endl;
 
 		cout << "3. WMB2x90 Missle Launcher" << endl;
 		cout << "Description: A salvo of Anti-Kaiju Missles" << endl;
 		cout << "Effect: Jaegar's AP +150" << endl;
-		cout << "XP in exchange: 70" << endl;
-		cout << endl;
+		cout << "XP in exchange: 70" << endl << endl;
 
 		cin >> choice;
 		
@@ -68,7 +64,7 @@ void jprops(Player1 &j){
 				break;
 
 			default:
-				cout << "Invalid input!"<< endl << "Please input a number between 1 - 3" << endl;
+				cout << "Invalid input!" << endl << "Please input a number between 1 - 3" << endl;
 				cout << "Your input is ";
 		
 			}
