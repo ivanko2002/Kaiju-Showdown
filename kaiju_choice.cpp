86fdd13d794d0e4b0786a6bc24f1cfde29487e1c
iivvaannkko <93305471+ngodelaynomore@users.noreply.github.com>
2021-12-01 15:15:26 +0800

    Update kaiju_choice.cpp

--- kaiju_choice.cpp
+++ kaiju_choice.cpp
@@ -13,32 +13,42 @@ string kchoice()
 	while (true)
 	{
 
-		cout << "Player 2 choose a kaiju" << endl;
-		cout << "1. Slattern" << endl;
+		cout << "Player 2 choose a kaiju" << endl << endl;
+		cout << "1. Slattern" << endl << endl;
 		cout << "Characteristics: Highest health points" << endl;
-		cout << "hp = 2500\nap = 75\nxp = 100\ndp = 10\nsp = 50" << endl
-			 << endl;
-
-		cout << "2. Otachi" << endl;
-		cout << "Characteristic: Highest speed" << endl;
-		cout << "hp = 2000\nap = 50\nxp = 150\ndp = 15\nsp = 180" << endl
-			 << endl;
-
-		cout << "3. Scunner" << endl;
-		cout << "Characteristic: Highest damage of skills" << endl;
-		cout << "hp = 1800\nap = 60\nxp = 120\ndp = 20\nsp = 80" << endl
-			 << endl;
-
-		cout << "4. Insurrector" << endl;
-		cout << "Characteristic: Highest attack power" << endl;
-		cout << "hp = 1200\nap = 120\nxp = 100\ndp = 10\nsp = 95" << endl
-			 << endl;
-
-		cout << "5. Atticon" << endl;
-		cout << "Characteristic: Highest defense point" << endl;
-		cout << "hp = 1300\nap = 30\nxp = 175\ndp = 30\nsp = 100" << endl
-			 << endl;
-
+		cout << "hp = 2500\tap = 75\txp = 100\tdp = 10\tsp = 50" << endl << endl;
+		cout << "skill_1 = Hammer Jaw\nxp = 15\tdamage = 50" << endl << endl;
+		cout << "skill_2 = Spinejackal\nxp = 35\tdamage = 100" << endl << endl;
+		cout << "skill_3 = Slattern Strike\nxp = 150\tdamage = 250" << endl << endl;
+		
+		cout << "2. Otachi" << endl << endl;
+		cout << "Characteristic: Highest speed" << endl << endl;
+		cout << "hp = 2000\nap = 50\txp = 150\tdp = 15\tsp = 180" << endl << endl;
+		cout << "skill_1 = Prehensile Tail\nxp = 15\tdamage = 35" << endl << endl;
+		cout << "skill_2 = Acid Sack\nxp = 40\tdamage = 125" << endl << endl;
+		cout << "skill_3 = Otachi Strike\nxp = 180\tdamage = 300" << endl << endl;
+		
+		cout << "3. Scunner" << endl << endl;
+		cout << "Characteristic: Highest damage of skills" << endl << endl;
+		cout << "hp = 1800\tap = 60\txp = 120\tdp = 20\tsp = 80" << endl << endl;
+		cout << "skill_1 = Clawhook\nxp = 25\tdamage = 50" << endl << endl;
+		cout << "skill_2 = Meathead\nxp = 60\tdamage = 160" << endl << endl;
+		cout << "skill_3 = Scunner Fury\nxp = 200\tdamage = 350" << endl << endl;
+		
+		cout << "4. Insurrector" << endl << endl;
+		cout << "Characteristic: Highest attack power" << endl << endl;
+		cout << "hp = 1200\tap = 120\txp = 100\tdp = 10\tsp = 95" << endl << endl;
+		cout << "skill_1 = Insurretor Claw\nxp = 30\tdamage = 130" << endl << endl;
+		cout << "skill_2 = Hammerjaw\nxp = 80\tdamage = 180" << endl << endl;
+		cout << "skill_3 = Insurrection\nxp = 250\tdamage = 550" << endl << endl;
+		
+		cout << "5. Atticon" << endl << endl;
+		cout << "Characteristic: Highest defense point" << endl << endl;
+		cout << "hp = 1300\tap = 30\txp = 175\tdp = 30\tsp = 100" << endl << endl;
+		cout << "skill_1 = Cherno Alpha\nxp = 40\tdamage = 60" << endl << endl;
+		cout << "skill_2 = Belobog\nxp = 75\tdamage = 160" << endl << endl;
+		cout << "skill_3 = Itak\nxp = 225\tdamage = 400" << endl << endl;
+		
 		cout << endl;
 
 		cout << "Please input a number between 1 to 5: ";
