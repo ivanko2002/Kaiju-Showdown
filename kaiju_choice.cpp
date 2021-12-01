6e505fcd62b4bfd28939dce4708db7d6f09c2ec0
Yu Hin <93305800+yhc369@users.noreply.github.com>
2021-12-01 14:59:35 +0800

    Update kaiju_choice.cpp

--- kaiju_choice.cpp
+++ kaiju_choice.cpp
@@ -1,52 +1,60 @@
 // Function for player to choosing a kaiju
-// Take number as input and return the corresponding kaiju 
+// Take number as input and return the corresponding kaiju
 
 #include <iostream>
 #include "struct.h"
 
 using namespace std;
 
-string kchoice(){
-	
-    int n;
-    while (true){
-	    
-        cout << "Player 2 choose a kaiju" << endl;
-        cout << "1. Slattern" << endl;
-	cout << "Characteristics: Highest health points" << endl;
-	cout << "hp = 2500\nap = 75\nxp = 100\ndp = 10\nsp = 50" << endl << endl;
+string kchoice()
+{
 
-        cout << "2. Otachi" << endl;
-	cout << "Characteristic: Highest speed" << endl;
-	cout << "hp = 2000\nap = 50\nxp = 150\ndp = 15\nsp = 180" << endl << endl;
+	int n;
+	while (true)
+	{
 
-        cout << "3. Scunner" << endl;
-	cout << "Characteristic: Highest damage of skills" << endl;
-	cout << "hp = 1800\nap = 60\nxp = 120\ndp = 20\nsp = 80" << endl << endl;
+		cout << "Player 2 choose a kaiju" << endl;
+		cout << "1. Slattern" << endl;
+		cout << "Characteristics: Highest health points" << endl;
+		cout << "hp = 2500\nap = 75\nxp = 100\ndp = 10\nsp = 50" << endl
+			 << endl;
 
-	cout << "4. Insurrector" << endl;
-	cout << "Characteristic: Highest attack power" << endl;
-	cout << "hp = 1200\nap = 120\nxp = 100\ndp = 10\nsp = 95" << endl << endl;
+		cout << "2. Otachi" << endl;
+		cout << "Characteristic: Highest speed" << endl;
+		cout << "hp = 2000\nap = 50\nxp = 150\ndp = 15\nsp = 180" << endl
+			 << endl;
 
-	cout << "5. Atticon" << endl;
-	cout << "Characteristic: Highest defense point" << endl;
-	cout << "hp = 1300\nap = 30\nxp = 175\ndp = 30\nsp = 100" << endl << endl;
-	
-	cout << endl;
+		cout << "3. Scunner" << endl;
+		cout << "Characteristic: Highest damage of skills" << endl;
+		cout << "hp = 1800\nap = 60\nxp = 120\ndp = 20\nsp = 80" << endl
+			 << endl;
 
-        cout << "Please input a number between 1 to 5: "; 
-	    
-        cin >> n;
+		cout << "4. Insurrector" << endl;
+		cout << "Characteristic: Highest attack power" << endl;
+		cout << "hp = 1200\nap = 120\nxp = 100\ndp = 10\nsp = 95" << endl
+			 << endl;
 
-	switch (n){
+		cout << "5. Atticon" << endl;
+		cout << "Characteristic: Highest defense point" << endl;
+		cout << "hp = 1300\nap = 30\nxp = 175\ndp = 30\nsp = 100" << endl
+			 << endl;
+
+		cout << endl;
+
+		cout << "Please input a number between 1 to 5: ";
+
+		cin >> n;
+
+		switch (n)
+		{
 		case 1:
 			return "Slattern";
 			break;
-		       
+
 		case 2:
 			return "Otachi";
 			break;
-		
+
 		case 3:
 			return "Scunner";
 			break;
@@ -60,11 +68,9 @@ string kchoice(){
 			break;
 
 		default:
-			cout << "Invalid input!"<< endl << "Please input a number between 1 - 5" << endl;
+			cout << "Invalid input!" << endl
+				 << "Please input a number between 1 - 5" << endl;
 			cout << "Your input is ";
+		}
 	}
-    }
 }
-
-
-
