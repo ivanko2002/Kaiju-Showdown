4fa5624f5449ec1b4f7cb5850c71e684d2ed4ed3
Yu Hin <93305800+yhc369@users.noreply.github.com>
2021-12-01 15:34:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,18 +32,19 @@ int main()
 				system("pause");
 				break;
 			case 3:
-				cout << "Jaegar data:" << endl << endl;
+				cout << "Jaegar data:" << endl
+					 << endl;
 				cout << "1. Gipsy Danger " << endl
 					 << endl;
 				cout << "Characteristics: Highest health points" << endl
 					 << endl;
 				cout << "hp = 1500\tap = 50\txp = 100\tdp = 20\tsp = 100" << endl
 					 << endl;
-				cout << "skill_1 = Elbow Rocket\nxp = 20\tdamage = 100" << endl
+				cout << "Skill 1 = Elbow Rocket\nxp = 20\tdamage = 100" << endl
 					 << endl;
-				cout << "skill_2 = Palm Grip Backslash\nxp = 40\tdamage = 150" << endl
+				cout << "Skill 2 = Palm Grip Backslash\nxp = 40\tdamage = 150" << endl
 					 << endl;
-				cout << "skill_3 = Plasma Cannon\nxp = 150\tdamage = 300" << endl
+				cout << "Skill 3 = Plasma Cannon\nxp = 150\tdamage = 300" << endl
 					 << endl;
 
 				cout << "2. Striker Eureka" << endl
@@ -52,11 +53,11 @@ int main()
 					 << endl;
 				cout << "hp = 1200\tp = 45\txp = 150\tdp = 10\tsp = 150" << endl
 					 << endl;
-				cout << "skill_1 = AKM Chest Launcher\nxp = 15\tdamage = 120" << endl
+				cout << "Skill 1 = AKM Chest Launcher\nxp = 15\tdamage = 120" << endl
 					 << endl;
-				cout << "skill_2 = Sting-Blades\nxp = 45\tdamage = 200" << endl
+				cout << "Skill 2 = Sting-Blades\nxp = 45\tdamage = 200" << endl
 					 << endl;
-				cout << "skill_3 = Nuclear Payload\nxp = 200\tdamage = 500" << endl
+				cout << "Skill 3 = Nuclear Payload\nxp = 200\tdamage = 500" << endl
 					 << endl;
 
 				cout << "3. Crimson Typhoon" << endl
@@ -65,11 +66,11 @@ int main()
 					 << endl;
 				cout << "hp = 1000\tap = 50\txp = 120\ndp = 30\tsp = 160" << endl
 					 << endl;
-				cout << "skill_1 = Jet Kick\nxp = 50\tdamage = 175" << endl
+				cout << "Skill 1 = Jet Kick\nxp = 50\tdamage = 175" << endl
 					 << endl;
-				cout << "skill_2 = Sterno Piston\nxp = 70\tdamage = 250" << endl
+				cout << "Skill 2 = Sterno Piston\nxp = 70\tdamage = 250" << endl
 					 << endl;
-				cout << "skill_3 = Thunder Cloud Formation\nxp = 300\tdamage = 800" << endl
+				cout << "Skill 3 = Thunder Cloud Formation\nxp = 300\tdamage = 800" << endl
 					 << endl;
 
 				cout << "4. Mammoth Apostle" << endl
@@ -78,11 +79,11 @@ int main()
 					 << endl;
 				cout << "hp = 1150\tap = 40\txp = 135\tdp = 55\tsp = 175" << endl
 					 << endl;
-				cout << "skill_1 = Apolo Punch\nxp = 35\tdamage = 125" << endl
+				cout << "Skill 1 = Apolo Punch\nxp = 35\tdamage = 125" << endl
 					 << endl;
-				cout << "skill_2 = Mammoth Slash\nxp = 55\tdamage = 225" << endl
+				cout << "Skill 2 = Mammoth Slash\nxp = 55\tdamage = 225" << endl
 					 << endl;
-				cout << "skill_3 = MA Salvation\nxp = 285\tdamage = 755" << endl
+				cout << "Skill 3 = MA Salvation\nxp = 285\tdamage = 755" << endl
 					 << endl;
 
 				cout << "5. Hydra Hyperion" << endl
@@ -91,54 +92,71 @@ int main()
 					 << endl;
 				cout << "hp = 950\tap = 100\txp = 150\tdp = 25\tsp = 200" << endl
 					 << endl;
-				cout << "skill_1 = Hydra Beacham\nxp = 30\tdamage = 150" << endl
+				cout << "Skill 1 = Hydra Beacham\nxp = 30\tdamage = 150" << endl
 					 << endl;
-				cout << "skill_2 = Hyper Jump\nxp = 275\tdamage = 300" << endl
+				cout << "Skill 2 = Hyper Jump\nxp = 275\tdamage = 300" << endl
 					 << endl;
-				cout << "skill_3 = Shatterdome\nxp = 350\tdamage = 950" << endl
+				cout << "Skill 3 = Shatterdome\nxp = 350\tdamage = 950" << endl
 					 << endl;
-					
-				cout << "Kaiju data:" << endl << endl;
-				cout << "1. Slattern" << endl << endl;
+
+				cout << "Kaiju data:" << endl
+					 << endl;
+				cout << "1. Slattern" << endl
+					 << endl;
 				cout << "Characteristics: Highest health points" << endl;
 				cout << "hp = 2500\nap = 75\nxp = 100\ndp = 10\nsp = 50" << endl
 					 << endl;
-				cout << "skill_1 = Hammer Jaw\nxp = 15\tdamage = 50" << endl << endl;
-				cout << "skill_2 = Spinejackal\nxp = 35\tdamage = 100" << endl << endl;
-				cout << "skill_3 = Slattern Strike\nxp = 150\tdamage = 250" << endl << endl;
-				
+				cout << "Skill 1 = Hammer Jaw\nxp = 15\tdamage = 50" << endl
+					 << endl;
+				cout << "Skill 2 = Spinejackal\nxp = 35\tdamage = 100" << endl
+					 << endl;
+				cout << "Skill 3 = Slattern Strike\nxp = 150\tdamage = 250" << endl
+					 << endl;
+
 				cout << "2. Otachi" << endl;
 				cout << "Characteristic: Highest speed" << endl;
 				cout << "hp = 2000\nap = 50\nxp = 150\ndp = 15\nsp = 180" << endl
 					 << endl;
-				cout << "skill_1 = Prehensile Tail\nxp = 15\tdamage = 35" << endl << endl;
-				cout << "skill_2 = Acid Sack\nxp = 40\tdamage = 125" << endl << endl;
-				cout << "skill_3 = Otachi Strike\nxp = 180\tdamage = 300" << endl << endl;
+				cout << "Skill 1 = Prehensile Tail\nxp = 15\tdamage = 35" << endl
+					 << endl;
+				cout << "Skill 2 = Acid Sack\nxp = 40\tdamage = 125" << endl
+					 << endl;
+				cout << "Skill 3 = Otachi Strike\nxp = 180\tdamage = 300" << endl
+					 << endl;
 
 				cout << "3. Scunner" << endl;
 				cout << "Characteristic: Highest damage of skills" << endl;
 				cout << "hp = 1800\nap = 60\nxp = 120\ndp = 20\nsp = 80" << endl
 					 << endl;
-				cout << "skill_1 = Clawhook\nxp = 25\tdamage = 50" << endl << endl;
-				cout << "skill_2 = Meathead\nxp = 60\tdamage = 160" << endl << endl;
-				cout << "skill_3 = Scunner Fury\nxp = 200\tdamage = 350" << endl << endl;
+				cout << "Skill 1 = Clawhook\nxp = 25\tdamage = 50" << endl
+					 << endl;
+				cout << "Skill 2 = Meathead\nxp = 60\tdamage = 160" << endl
+					 << endl;
+				cout << "Skill 3 = Scunner Fury\nxp = 200\tdamage = 350" << endl
+					 << endl;
 
 				cout << "4. Insurrector" << endl;
 				cout << "Characteristic: Highest attack power" << endl;
 				cout << "hp = 1200\nap = 120\nxp = 100\ndp = 10\nsp = 95" << endl
 					 << endl;
-				cout << "skill_1 = Insurretor Claw\nxp = 30\tdamage = 130" << endl << endl;
-				cout << "skill_2 = Hammerjaw\nxp = 80\tdamage = 180" << endl << endl;
-				cout << "skill_3 = Insurrection\nxp = 250\tdamage = 550" << endl << endl;
+				cout << "Skill 1 = Insurretor Claw\nxp = 30\tdamage = 130" << endl
+					 << endl;
+				cout << "Skill 2 = Hammerjaw\nxp = 80\tdamage = 180" << endl
+					 << endl;
+				cout << "Skill 3 = Insurrection\nxp = 250\tdamage = 550" << endl
+					 << endl;
 
 				cout << "5. Atticon" << endl;
 				cout << "Characteristic: Highest defense point" << endl;
 				cout << "hp = 1300\nap = 30\nxp = 175\ndp = 30\nsp = 100" << endl
 					 << endl;
-				cout << "skill_1 = Cherno Alpha\nxp = 40\tdamage = 60" << endl << endl;
-				cout << "skill_2 = Belobog\nxp = 75\tdamage = 160" << endl << endl;
-				cout << "skill_3 = Itak\nxp = 225\tdamage = 400" << endl << endl;
-				
+				cout << "Skill 1 = Cherno Alpha\nxp = 40\tdamage = 60" << endl
+					 << endl;
+				cout << "Skill 2 = Belobog\nxp = 75\tdamage = 160" << endl
+					 << endl;
+				cout << "Skill 3 = Itak\nxp = 225\tdamage = 400" << endl
+					 << endl;
+
 				system("pause");
 				break;
 			case 4:
